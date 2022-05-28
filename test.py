e93387cc05329ed76b1a0f92d211b1fd431ad8c8
Jakub Jira <jakub.jira@protonmail.com>
2022-05-28 16:11:47 +0200

    basic echoing tokio server working

---
+++ test.py
@@ -0,0 +1,16 @@
+import socket
+target_host = "0.0.0.0"
+target_port = 8123
+# create a socket connection
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# let the client connect
+client.connect((target_host, target_port))
+# send some data
+while True:
+    blob = input()
+    if blob == "exit()":
+        break
+    client.send(bytes(blob, "UTF-8"))
+    # get some data
+    response = client.recv(4096)
+    print(response)
